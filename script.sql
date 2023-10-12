c0708080a07ad03c54c82513d6458c409bbd4654
marcoweb <marcocarvalho.web@gmail.com>
2023-10-12 01:47:52 +0000

    Aula do dia 11/10/2023

---
+++ script.sql
@@ -0,0 +1,10 @@
+-- Exibir os bancos de dados do servidor
+# Comentário (MySQL)
+/* Comentário de várias linhas (MySQL) */
+SHOW DATABASES;
+
+-- Exibir tabelas de banco de dados específico
+SHOW TABLES FROM information_schema;
+
+-- Criaçao de nova base de dados
+CREATE DATABASE banco_de_teste;
