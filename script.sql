1c1a60bf09fae11b898aa44e7f4a22a7c8b2c1e0
Marco Antônio Gusmão Carvalho <marcocarvalho.web@gmail.com>
2023-11-02 01:45:35 +0000

    Aula 01/11/2023

--- script.sql
+++ script.sql
@@ -8,3 +8,24 @@ SHOW TABLES FROM information_schema;
 
 -- Criaçao de nova base de dados
 CREATE DATABASE banco_de_teste;
+
+-- Remove uma base de dados
+
+DROP DATABASE banco_de_teste;
+
+CREATE TABLE banco_de_teste.alunos(
+    id INT PRIMARY KEY,
+    nome VARCHAR(255)
+);
+
+SHOW TABLES FROM banco_de_teste;
+
+SHOW FIELDS FROM banco_de_teste.alunos;
+
+USE banco_de_teste;
+
+CREATE TABLE teste(
+    id INT PRIMARY KEY,
+    valor CHAR(11)
+)
+
